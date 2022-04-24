113e66132b09dd7c5c1897e634c6d1f1caabb9aa
JohanAraya <johan.araya@hotmail.es>
2022-04-23 20:20:23 -0600

    Se agrega el Back end del proyecto

---
+++ Pojos/Interfaces/IListar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pojos.Interfaces
+{
+    public interface IListar<TEntidad, TEntidadID>
+    {
+        List<TEntidad> Listar();
+
+        TEntidad seleccionarPorIdUsuario(string idUsuario);
+
+        TEntidad seleccionarPorId(TEntidadID entidadId);
+    }
+
+
+}
